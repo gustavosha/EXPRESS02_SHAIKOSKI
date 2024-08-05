9467ddab6f35be8870b47eba35f0624e7e470f17
gustavosha <116170062+gustavosha@users.noreply.github.com>
2024-08-05 10:59:52 -0300

    Add files via upload

---
+++ EXPRESS02_SHAIKOSKI/server3.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app =  express()
+
+app.listen(3000,()=>{
+    console.log('Servidor em execução')
+});
+
+//rotas com parâmetros
+
+app.get('/',(req,res)=>{
+    res.send('Olá, mundo!')
+})
+
+//Rota com um parâmetro
+app.get('/ola/:nome',(req,res)=>{
+    console.log(req.params.nome)
+    res.send('Olá '+req.params.nome)
+} )
+
+//Rota com 2 parãmetros
+app.get('/ola/:nome/:empresa',(req,res)=>{
+    res.send('Ola '+req.params.nome+ " da empresa "+req.params.empresa)
+})
+
+//Rota com soma de dois parâmetros
+app.get('/soma/:a/:b',(req,res)=>{
+    res.send('O resultado da soma de: '+req.params.a+' é '+ req.params.b+ ' e: '+(parseInt(req.params.b)))
+})
